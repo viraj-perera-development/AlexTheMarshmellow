461352c80abe4810b1ebb36148bc9cf77f5fdf49
Viraj Perera <vperera@care.local>
2023-12-18 17:32:37 +0100

    up date the function counter but sill dosen't work

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,26 +2,30 @@ import React, { useState } from 'react';
 import img1 from '../assets/img1.png';
 import img2 from '../assets/img2.png';
 import img3 from '../assets/img3.png';
-import img4 from '../assets/img4.png';
-import img5 from '../assets/img5.png';
-import img6 from '../assets/img6.png';
+import image1 from '../assets/img4.png';
+import image2 from '../assets/img5.png';
+import image3 from '../assets/img6.png';
 
 function Home() {
   const [counterLeft, setCounterLeft] = useState(4);
   const [counterRight, setCounterRight] = useState(0);
 
-  const imgSequenceLeft = [img1, img2, img3];
-  const imgSequenceRight = [img4, img5, img6];
+  const bedReaction = [img1, img2, img3];
+  const goodReaction = [image1, image2, image3];
 
   const handleFlip = (direction) => {
     if (direction === 'left') {
-      setCounterLeft((counterLeft) => (counterLeft + 1) % imgSequenceLeft.length);
-      console.log(imgSequenceLeft[counterLeft]);
+      setCounterLeft((counterLeft) => (counterLeft + 1) % bedReaction.length);
+      console.log(bedReaction[counterLeft]);
     } else if (direction === 'right') {
-      setCounterRight((prevCounter) => (prevCounter + 1) % imgSequenceRight.length);
-      console.log(imgSequenceLeft[counterRight]);
+      setCounterRight((prevCounter) => {
+        const nextCounter = prevCounter + 1;
+        return nextCounter < goodReaction.length ? nextCounter : prevCounter;
+      });
+      console.log(goodReaction[counterRight]);
     }
-  };
+  };  
+  
 
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
@@ -29,10 +33,10 @@ function Home() {
         <h1 className="title mb-4">Do you like Marshmallow?</h1>
         <div className={`image-container ${'flipped'}`}>
           <div className="image-front">
-            <img src={imgSequenceLeft[counterLeft]} className="img-fluid w-75" alt="Front Image" />
+            <img src={bedReaction[counterLeft]} className="img-fluid w-75" alt="Front Image" />
           </div>
           <div className="image-back">
-            <img src={imgSequenceRight[counterRight]} className="img-fluid w-75" alt="Back Image" />
+            <img src={goodReaction[counterRight]} className="img-fluid w-75" alt="Back Image" />
           </div>
         </div>
         <div className="buttons-container mt-4">
